474f0a5c39962dd46bf52b20ae4fc9f73e03dccb
Jordan <synesthesia57@gmail.com>
2020-02-21 19:32:51 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MyAngularClient';
+export class AppComponent implements OnInit {
+  
+  public title = 'Jordan\'s app';
+  public isAuthenticated: boolean;
+
+  constructor(public oktaAuth: OktaAuthService ) {
+    this.oktaAuth.$authenticationState.subscribe((isAuthenticated: boolean) => this.isAuthenticated = isAuthenticated);
+  }
+
+
+ngOnInit() {
+  this.oktaAuth.isAuthenticated().then((auth) => {this.isAuthenticated = auth;
+  });
+}
+
+  login(){
+    this.oktaAuth.loginRedirect();
+
+  }
+
+  logout(){
+    this.oktaAuth.logout('/');
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FlexLayoutModule } from '@angular/flex-layout';
+import { OktaAuthModule } from '@okta/okta-angular';
+
+import {
+  MatButtonModule,
+  MatDividerModule,
+  MatIconModule,
+  MatMenuModule,
+  MatProgressSpinnerModule,
+  MatTableModule,
+  MatToolbarModule,
+} from '@angular/material';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatMenuModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatToolbarModule,
+    HttpClientModule,
+    FormsModule,
+    OktaAuthModule.initAuth({
+      issuer: 'https://dev-676714.okta.com/oauth2/default',
+      redirectUri: 'http://localhost:4200/implicit/callback',
+      clientId: '0oa2a2mgv0TvUnsQJ4x6'
+
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
